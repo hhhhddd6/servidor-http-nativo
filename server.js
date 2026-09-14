089f5411c53ff78696a8164f0ffa68aa19efd2b3
hhhhddd6 <heitor.moreira@estudante.ifms.edu.br>
2026-09-14 19:31:19 +0000

    edit: mudei o codigo

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
-const http = require('node:http')
+import http from 'node:http'
+import { url } from 'node:inspector';
+import { URL } from 'node:url'
 
-const porta = 3000
+const port = 3000
 
-const server = http.createServer();
+const status = {
+    "status": "ok",
+    "date": new Date().toISOString()
+}
 
-server.on('request', (req, res) => {
-    console.log(`Requisição recebida! ${req.method} ${req.url}`);
+const produtos = [
+    { id: 1, nome: "Sabonete" },
+    { id: 2, nome: "Monitor" },
+    { id: 3, nome: "Cadeira Gamer" }
+]
 
-    res.statusCode = 201
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.end("Recurso criado!");
-    console.log(new Date().toISOString());
+
+const server = http.createServer((req, res) => {
+    console.log(`Método: ${req.method} | URL:${req.url}`)
+    const urlObj = new URL(req.url, `http://${req.headers.host}`)
+    res.statusCode= 404;
+    res.setHeader('Content-Type', 'application/json');
+
+    if (req.method == "GET" && urlObj.pathname == "/contato") {
+        res.statusCode = 200;
+        return res.end(JSON.stringify({
+            "numero_telefone": "67 99999-9999",
+            "endereco": "Rua da Alegria, 99"
+        })
+    );
+    }
+
+    if (req.method == "GET" && urlObj.pathname == "/produtos") {
+        res.statusCode = 200;
+        return res.end(JSON.stringify(produtos));
+    }
+
+    res.end(JSON.stringify({ "data": "Página Inexistente" }));
 });
 
-server.listen(porta, ()=> {
-    console.log(`Servidor ouvindo na porta ${porta}`)
+server.listen(port, () => {
+    console.log("Servidor funcionando na porta ", port)
 });
